523ef8dc5cd2d82a22d23fabf14c9762bfdffdbc
Shivam Mishra <63857928+ShivamYash742@users.noreply.github.com>
2024-12-23 15:38:16 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import Header from './Compenets/Header'
-import Home from './Compenets/Home'
-import  About from './Compenets/About'
-import Footer from './Compenets/Footer'
-import Services from './Compenets/Services'
-import Skills from './Compenets/Skills'
+import Header from "./Compenets/Header";
+import Home from "./Compenets/Home";
+import About from "./Compenets/About";
+import Footer from "./Compenets/Footer";
+import Services from "./Compenets/Services";
+import Skills from "./Compenets/Skills";
 
-import './new.css'
+import "./new.css";
 
 function App() {
-
   return (
     <>
-    <Header />
-    <Home></Home>
-    <About></About>
-    <Services></Services>
-    <Skills></Skills>
-    <Footer></Footer>
-    
-      
+      <Header />
+      <Home></Home>
+      <About></About>
+      <Services></Services>
+      <Skills></Skills>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
